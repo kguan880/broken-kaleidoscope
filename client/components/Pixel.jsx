bb095f097b8de1cf41b3d112bcedfe55cd46fd26
Simon Hall <simon970@gmail.com>
2020-04-07 11:50:00 +1200

    add state to control pixel style

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    state = {
+        style: {
+            height: '20px',
+            width: '20px',
+            backgroundColor: 'blue'
+            }
+
+    }
+    render() {
+        return(
+            <div style={this.state.style}>
+            </div>
+        )
+    }
+}
+
+export default Pixel
