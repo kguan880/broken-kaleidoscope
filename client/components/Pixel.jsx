eddc34ae7eab2bce4347f83dcc63fbbf70ae427b
Simon Hall <simon970@gmail.com>
2020-04-07 11:58:56 +1200

    randomize pixel colour

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -5,10 +5,11 @@ class Pixel extends React.Component {
         style: {
             height: '20px',
             width: '20px',
-            backgroundColor: 'blue'
+            backgroundColor:  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
             }
-
     }
+
+
     render() {
         return(
             <div style={this.state.style}>
