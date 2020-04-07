8128259167597cd2eda74639a40b15abef6b3779
Simon Hall <simon970@gmail.com>
2020-04-07 12:13:59 +1200

    udpate style on click - almost working

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 class Pixel extends React.Component {
+    // const randomHexColour = 
+    
     state = {
         style: {
             height: '20px',
@@ -9,10 +11,17 @@ class Pixel extends React.Component {
             }
     }
 
+    clickHandler = evt => {
+        this.setState({
+          style: {
+              backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` 
+            }
+        })
+      }
 
     render() {
         return(
-            <div style={this.state.style}>
+            <div onClick={this.clickHandler} style={this.state.style}>
             </div>
         )
     }
