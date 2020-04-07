67271bb28852a7da113387a6d3383e2c2588ef4b
Josh Wood <JHW300@gmail.com>
2020-04-07 13:24:04 +1200

    Multiply Pixels

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    render() {
+        return (
+            <div style={{
+                height: '50px',
+                width: '50px',
+                backgroundColor: 'cornflowerblue'
+            }}>
+            </div>
+        )
+    }
+}
+    
+
+
+export default Pixel
