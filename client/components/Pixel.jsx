583c678303db5a6ac78d4e831c7ab021d20c22af
Josh Wood <JHW300@gmail.com>
2020-04-07 13:40:50 +1200

    Added state component

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 class Pixel extends React.Component {
-    render() {
-        return (
-            <div style={{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            style: {
                 height: '50px',
                 width: '50px',
                 backgroundColor: 'cornflowerblue'
-            }}>
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div style={this.state.style}>
             </div>
         )
     }
