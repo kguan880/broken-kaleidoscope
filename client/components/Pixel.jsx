440c6cefa5440652e0d00d05261ca3afc9992002
Simon Hall <simon970@gmail.com>
2020-04-07 13:22:35 +1200

    create additional event handlers

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -16,14 +16,56 @@ class Pixel extends React.Component {
           style: {
             height: '20px',
             width: '20px',
-            backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}` 
+            backgroundColor: `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+            }
+        })
+      }
+      hoverHandler = evt => {
+        this.setState({
+          style: {
+            height: '20px',
+            width: '20px',
+            backgroundColor: 'green' 
+            }
+        })
+      }
+      dragHandler = evt => {
+        this.setState({
+          style: {
+            height: '20px',
+            width: '20px',
+            backgroundColor: `yellow` 
+            }
+        })
+      }
+      dblClickHandler = evt => {
+        this.setState({
+          style: {
+            height: '20px',
+            width: '20px',
+            backgroundColor: `white` 
+            }
+        })
+      }
+      rightClickHandler = evt => {
+        this.setState({
+          style: {
+            height: '20px',
+            width: '20px',
+            backgroundColor: `black` 
             }
         })
       }
 
     render() {
         return(
-            <div onClick={this.clickHandler} style={this.state.style}>
+            <div 
+            onClick={this.clickHandler}
+            onContextMenu={this.rightClickHandler}
+            onDoubleClick={this.dblClickHandler}
+            onMouseEnter={this.hoverHandler}
+            onDragEnter={this.dragHandler}
+             style={this.state.style}>
             </div>
         )
     }
